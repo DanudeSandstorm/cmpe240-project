36d2daae1c41c13f1d99a231e92ef444fa99de96
Daniel <dds2130@rit.edu>
2016-11-16 20:14:00 -0500

    templating

---
+++ source/tmod.c
@@ -0,0 +1,15 @@
+#include "tmod.h"
+#include "ledmod.h"
+#include "printf.h"
+
+int translate(const char* string) {
+    //TODO
+    //to_uppercase
+    //check against 
+    return 0;
+}
+
+int print_translation(const char* string) {
+    //TODO format output
+    return 0;
+}
